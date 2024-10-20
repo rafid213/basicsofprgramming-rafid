c861d916c0873f2583baf132cd4636617a6c24a9
rafid213 <mdrafidislam23@gmail.com>
2024-10-20 09:10:42 +0300

    Finding the Greater Number

---
+++ basics/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+
+        int num1 = 120;
+        int num2 = 100;
+
+        if (num1 > num2) {
+
+            System.out.println("120 is greater than 100");
+            
+        }
+
+
+
+    }
+}
